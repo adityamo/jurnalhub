fdb0b3e867105fc6413198724970dd2dad0e4ac9
aditya.septama <rfadityahalim18@gmail.com>
2026-02-03 09:44:58 +0700

    feat:revision_mobile_version_tools

--- src/modules/landing/ToolsSection.tsx
+++ src/modules/landing/ToolsSection.tsx
@@ -27,9 +27,9 @@ const ToolsSection = () => {
           <div className="max-w-5xl px-4 xl:px-0 py-10 mx-auto">
             <div className="border border-line-2 rounded-xl">
               <div className="p-4 lg:p-8 bg-linear-to-bl from-layer via-background to-background-1 rounded-xl">
-                <div className="grid grid-cols-1 sm:grid-cols-3 items-center gap-y-20 gap-x-12">
+                <div className="grid grid-cols-1 sm:grid-cols-3 items-center gap-y-10 lg:gap-y-20 gap-x-12">
                   {/* Stats */}
-                  <div className="relative text-center first:before:hidden before:absolute before:-top-full sm:before:top-1/2 before:start-1/2 sm:before:-start-6 before:h-20 before:border-s before:border-line-2 before:rotate-[60deg] sm:before:rotate-12 before:transform sm:before:-translate-y-1/2 before:-translate-x-1/2 sm:before:-translate-x-0 before:mt-3.5 sm:before:mt-0">
+                  <div className="relative text-center first:before:hidden before:hidden sm:before:block before:absolute before:-top-full sm:before:top-1/2 before:start-1/2 sm:before:-start-6 sm:before:h-20 sm:before:border-s sm:before:border-line-2 before:rotate-[60deg] sm:before:rotate-12 before:transform sm:before:-translate-y-1/2 before:-translate-x-1/2 sm:before:-translate-x-0 before:mt-3.5 sm:before:mt-0">
                     <div className="flex justify-center items-center -space-x-5">
                       <Image
                         src="/assets/tools/grammarly.png"
@@ -37,13 +37,13 @@ const ToolsSection = () => {
                         width={800}
                         height={500}
                         priority
-                        className="w-30 lg:w-100 lg:h-auto h-auto"
+                        className="w-40 lg:w-100 lg:h-auto h-auto"
                       />
                     </div>
                   </div>
                   {/* End Stats */}
                   {/* Stats */}
-                  <div className="relative text-center first:before:hidden before:absolute before:-top-full sm:before:top-1/2 before:start-1/2 sm:before:-start-6 before:h-20 before:border-s before:border-line-2 before:rotate-[60deg] sm:before:rotate-12 before:transform sm:before:-translate-y-1/2 before:-translate-x-1/2 sm:before:-translate-x-0 before:mt-3.5 sm:before:mt-0">
+                  <div className="relative text-center first:before:hidden before:hidden sm:before:block before:absolute before:-top-full sm:before:top-1/2 before:start-1/2 sm:before:-start-6 sm:before:h-20 sm:before:border-s sm:before:border-line-2 before:rotate-[60deg] sm:before:rotate-12 before:transform sm:before:-translate-y-1/2 before:-translate-x-1/2 sm:before:-translate-x-0 before:mt-3.5 sm:before:mt-0">
                     <div className="flex justify-center items-center -space-x-5">
                       <Image
                         src="/assets/tools/turnity.png"
@@ -57,7 +57,7 @@ const ToolsSection = () => {
                   </div>
                   {/* End Stats */}
                   {/* Stats */}
-                  <div className="relative text-center first:before:hidden before:absolute before:-top-full sm:before:top-1/2 before:start-1/2 sm:before:-start-6 before:h-20 before:border-s before:border-line-2 before:rotate-[60deg] sm:before:rotate-12 before:transform sm:before:-translate-y-1/2 before:-translate-x-1/2 sm:before:-translate-x-0 before:mt-3.5 sm:before:mt-0">
+                  <div className="relative text-center first:before:hidden before:hidden sm:before:block before:absolute before:-top-full sm:before:top-1/2 before:start-1/2 sm:before:-start-6 sm:before:h-20 sm:before:border-s sm:before:border-line-2 before:rotate-[60deg] sm:before:rotate-12 before:transform sm:before:-translate-y-1/2 before:-translate-x-1/2 sm:before:-translate-x-0 before:mt-3.5 sm:before:mt-0">
                     <div className="flex justify-center items-center -space-x-5">
                       <Image
                         src="/assets/tools/mendeley.png"
